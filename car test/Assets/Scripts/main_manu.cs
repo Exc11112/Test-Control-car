cf19154b68dd58dad3a4355605df5725a75cd034
noManSky02 <167184044+noManSky02@users.noreply.github.com>
2024-11-29 20:12:39 +0700

    AddUI_main_manu
    add -start ui  -settting ui  -exit ui
      scripts main_manu
      new fodel  ui main manu
      new scenes start_manu

---
+++ car test/Assets/Scripts/main_manu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Load Scene ชื่อ "GameScene"
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenOptions()
+    {
+        // เปิดเมนู Options
+        Debug.Log("Options Menu Opened");
+    }
+
+    public void ExitGame()
+    {
+        // ปิดโปรแกรม
+        Debug.Log("Game Exited");
+        Application.Quit();
+    }
+}
